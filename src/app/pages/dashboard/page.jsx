c04447f9a8f1164365dc9265b0f0ea04f47eb378
Bsoluwanjoba <bsoluwanjoba@gmail.com>
2024-10-22 15:03:19 +0100

    feat: whoosh

--- src/app/pages/dashboard/page.jsx
+++ src/app/pages/dashboard/page.jsx
@@ -1,21 +1,21 @@
 'use client'
-// import { useRevenueData } from '../../hooks/useRevenueData';
+import { useRevenueData } from '../../hooks/useRevenueData';
 import Link from 'next/link';
 
 export default function page() {
-    // const { totalRevenue, loading, error } = useRevenueData();
+    const { totalRevenue, loading, error } = useRevenueData();
   return (
     <div classname="mt-5">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         <div className="bg-blue-100 p-8 rounded-lg">
           <h2 className="text-lg font-semibold mb-2">Total Revenue</h2>
-          {/* {loading ? (
+          {loading ? (
             <p>Loading...</p>
           ) : error ? (
             <p>Error: {error}</p>
           ) : (
             <p className="text-2xl font-bold">${totalRevenue.toLocaleString()}</p>
-          )} */}
+          )}
           <Link href="/dashboard/revenue" className="text-blue-500 hover:underline mt-2 inline-block">
             View Details
           </Link>

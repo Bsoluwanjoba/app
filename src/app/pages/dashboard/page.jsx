fa2a7d20b9129133ab9a50e5e1b4316c3163d842
Bsoluwanjoba <bsoluwanjoba@gmail.com>
2024-10-24 12:41:58 +0100

    ff

--- src/app/pages/dashboard/page.jsx
+++ src/app/pages/dashboard/page.jsx
@@ -9,13 +9,13 @@ export default function page() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         <div className="bg-blue-100 p-8 rounded-lg">
           <h2 className="text-lg font-semibold mb-2">Total Revenue</h2>
-          {loading ? (
+          {/* {loading ? (
             <p>Loading...</p>
           ) : error ? (
             <p>Error: {error}</p>
           ) : (
             <p className="text-2xl font-bold">${totalRevenue.toLocaleString()}</p>
-          )}
+          )} */}
           <Link href="/pages/dashboard/revenue" className="text-blue-500 hover:underline mt-2 inline-block">
             View Details
           </Link>
